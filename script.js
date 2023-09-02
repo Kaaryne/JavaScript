1d6a9658897c91ba42a254769d095bb6989720fd
Kaaryne <141533267+Kaaryne@users.noreply.github.com>
2023-09-02 19:04:15 +0000

    s

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-mensagem = "Olá"  /* atribuição de valor a variavel */
 
-alert(mensagem)   /* mensagem no site */
+/*
+mensagem = "Olá" 
+
+alert(mensagem)  
+*/
 
 var y;
 
@@ -20,12 +23,57 @@ y = null;
 document.write(y);
 document.write ("<br>");
 
+/* Expressões */
+
 var expressao;
 
 expressao = 30 + 20 - 10 * 5;
+document.write("30 + 20 - 10 * 5 = ");  /* expressão escrita, pois está entre aspas (string) */
+document.write(expressao);              /* resultado "0", pois é a variável */
 
-document.write("30 + 20 - 10 * 5 = ");
-document.write(expressao);
+/* Decisões */
 
+if (condição) {
+    declaracao_1;
+}   else {
+    declaracao_2;
+}
 
+/* Estruturas compostas */
 
+if (condicao) {
+    declaracao_1;
+}   else if (condicao_2) {
+    declaracao_2;
+}   else if (condicao_n) {
+    declaracao_n;
+}   else {
+    declaração_final;
+}
+
+/* Condições podem executar mais de 1 comando */
+
+if (condicao) {
+    declaracao_1_executada_se_condicao_for_verdadeira;
+    declaracao_2_executada_se_condicao_for_verdadeira;
+}   else {
+    declaracao_3_executada_se_condicao_for_falsa;
+    declaracao_4_executada_se_condicao_for_falsa;
+}
+
+/* Exemplo */
+
+var idade;
+idade = prompt("Digite a sua idade: ");
+
+if (idade >= 18) {
+    var maiorIdade = true;
+}   else {
+    var maiorIdade = false;
+}
+
+if (maiorIdade) {
+    document.write("Maior de idade, pode dirigir!");
+}   else {
+    document.write("Menor de idade. :(");
+}
