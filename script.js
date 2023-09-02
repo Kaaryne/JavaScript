b6d017d077889882f4b9ec711649ba2dd03d8fae
Kaaryne <141533267+Kaaryne@users.noreply.github.com>
2023-09-02 17:49:25 +0000

    a

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-mensagem = "Olá"
+mensagem = "Olá"  /* atribuição de valor a variavel */
 
-alert(mensagem)
+alert(mensagem)   /* mensagem no site */
+
+var y;
+
+y = 37;
+document.write(y);
+document.write ("<br>");
+
+y = "Karyne";
+document.write(y);
+document.write ("<br>");
+
+y = true;
+document.write(y);
+document.write ("<br>");
+
+y = null;
+document.write(y);
+document.write ("<br>");
+
+var expressao;
+
+expressao = 30 + 20 - 10 * 5;
+
+document.write("30 + 20 - 10 * 5 = ");
+document.write(expressao);
+
+
+
