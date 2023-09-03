7e2e8fc311f71d3dc26396e66e21adc20a806871
Kaaryne <kaarynefurlan@gmail.com>
2023-09-03 11:22:37 -0300

    aaa

--- script.js
+++ script.js
@@ -214,3 +214,26 @@
 // var idades = [25, 30, 32, 37];
 // idades.unshift(42);
 // alert(idades);
+
+/* Matriz */
+// var matint = [[3,6,9,],[2,4,6],[1,2,3]]
+
+// for(let lin=0 ; lin<3 ; lin++){
+//     for (let col=0; col<3; col++)
+//         document.write(matint[lin][col] + " ");
+
+//     document.write("<br>");
+// }
+
+// var notas = [[4.5 , 6.5 , 8.7],[8.5 , 9.7 , 9.9]]
+
+// var colunas = notas[0].length;
+
+// soma = 0;
+
+// for(let i=o ; i<colunas ; i++){
+//     soma = soma + notas[1][i];
+// }
+
+// var media = soma/colunas;
+// document.write("Mesia da Maria: " + media);
